518a4f63386e4c4c55e9b3fe5915ff16488170c5
Sobia Shafique <sobiashafique084@gmail.com>
2026-09-22 21:53:54 +0500

    average

---
+++ average.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void average(int [],int);      //function prototype
+int main()
+{
+	int size=5;
+	ave
+	int sum=0;
+	int array[size]={12,3,4,5,6,};
+	for(int i=0;i<5;i++)
+	{
+		
+		sum=sum+array[i];
+	}
+	int average=sum/size;
+	printf("%d",average);
+	return 0;
+	
+}
